b910db2783afc9958b6aace16f305e2d532c3722
newHoriz0n <paulbenz90@gmx.de>
2018-06-07 23:21:07 +0200

    Spieler

---
+++ SpaceStory/src/game/Spieler.java
@@ -0,0 +1,34 @@
+package game;
+
+import spacefight.schiff.Schiff;
+import topview.pbLib.Koordinate3D;
+
+public class Spieler {
+
+	private Schiff schiff;
+
+	public Spieler() {
+
+	}
+
+	public void setSchiff(Schiff s) {
+		this.schiff = s;
+	}
+
+	public Koordinate3D getSpielerPosition() {
+		return schiff.getPosition();
+	}
+
+	public void setZielAusrichtung(double richtung) {
+		schiff.setZielAusrichtung(richtung);
+	}
+
+	public void setLenkrichtung(int richtung) {
+		schiff.steuern(richtung);
+	}
+
+	public void schussAufZielPosition(int x, int y) {
+		schiff.schiesseAufPosition(x, y);
+	}
+
+}
